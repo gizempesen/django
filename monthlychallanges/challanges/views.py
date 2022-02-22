e0286cb8aefb0938541d011b82869f030720dc1f
gizem <pesengizem999@gmail.com>
2022-02-22 16:45:36 +0300

    all months

--- monthlychallanges/challanges/views.py
+++ monthlychallanges/challanges/views.py
@@ -2,5 +2,38 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse("This is the index page")
+def january(request):
+    return HttpResponse("This is the january page")
+
+def february(request):
+    return HttpResponse("This is the february page")
+
+def march(request):
+    return HttpResponse("This is the march page")
+
+def april(request):
+    return HttpResponse("This is the april page")
+
+def may(request):
+    return HttpResponse("This is the may page")
+
+def june(request):
+    return HttpResponse("This is the june page")
+
+def july(request):
+    return HttpResponse("This is the july page")
+
+def august(request):
+    return HttpResponse("This is the july page")
+
+def september(request):
+    return HttpResponse("This is the july page")
+
+def october(request):
+    return HttpResponse("This is the october page")
+
+def november(request):
+    return HttpResponse("This is the november page")
+
+def december(request):
+    return HttpResponse("This is the december page")
